e339821dd76e43cb2c8c4ebdc82c63236860c921
mutiarardsl <160991558+mutiarardsl@users.noreply.github.com>
2025-03-08 01:02:18 +0700

    Kalkulator Sederhana

---
+++ app/src/main/java/com/example/kalkulatorsederhana/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.kalkulatorsederhana
+
+import android.content.Intent;
+import android.os.Bundle
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.appcompat.app.AppCompatActivity;
+import com.example.kalkulatorsederhana.ui.theme.KalkulatorSederhanaTheme
+
+
+public class MainActivity extends AppCompatActivity{
+    private EditText etAngka1, etAngka2;
+    private Radiogroup rgOperasi;
+    private
+}
